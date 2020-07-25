f39ab6738dec6e56ceaac4b159fcbd9b71e32904
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-07-25 14:02:14 +0800

    Cancel request if user navigate to other page

--- hooks/useStock.js
+++ hooks/useStock.js
@@ -1,15 +1,28 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import api from '../apis';
 
 const useStock = (type = 'hst') => {
   const [data, setData] = useState({});
 
+  const CancelToken = axios.CancelToken;
+  const source = CancelToken.source();
+
   useEffect(() => {
     api
-      .get(`/stocks/${type}`)
+      .get(`/stocks/${type}`, { cancelToken: source.token })
       .then((res) => res.data)
       .then(setData)
-      .catch((err) => console.log(err));
+      .catch((thrown) => {
+        if (axios.isCancel(thrown)) {
+          console.log('Request canceled', thrown.message);
+        } else {
+          // handle error
+          console.log(thrown);
+        }
+      });
+
+    return () => source.cancel('Operation canceled by the user.');
   }, []);
 
   return data;
